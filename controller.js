223e84c50eca8818956220e2371fe132fd635361
Paul Acklam <paulacklam@gmail.com>
2018-09-07 17:06:19 +0100

    improved handling of requests with no number query

--- controller.js
+++ controller.js
@@ -14,45 +14,48 @@ const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
 const handleNumber = (req, res, next) => {
   const number = parseInt(req.query.number);
 
-  axios.get(`http://numbersapi.com/${number}`)
-    .then(function (responseTrivia) {
-      var parameters = {
-        'text': responseTrivia.data,
-        'features': {
-          'keywords': {
-            'emotion': false,
-            'sentiment': false,
-            'limit': 5
+  if (req.query.number === undefined) {
+    res.render('index');
+  } else {
+    axios.get(`http://numbersapi.com/${number}`)
+      .then(function (responseTrivia) {
+        var parameters = {
+          'text': responseTrivia.data,
+          'features': {
+            'keywords': {
+              'emotion': false,
+              'sentiment': false,
+              'limit': 5
+            }
           }
         }
-      }
-      naturalLanguageUnderstanding.analyze(parameters, function(error, responseWatson) {
-        if (error) {
-          console.log({error, msg: 'Error getting data from Watson'});
-        } else {
-          const giphyKeyword = responseWatson.keywords[0].text;
-          console.log(giphyKeyword);
-          axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${giphyAPIkey}&q=${giphyKeyword}&limit=1`)
-            .then(responseGiphy => {
-              console.log(responseGiphy.data.data[0].images.original.url);
-              const dataToRender = {
-                number,
-                fact: responseTrivia.data,
-                keyword: responseWatson.keywords[0].text,
-                // gifURL: responseGiphy.data.data[0].images.fixed_width.url
-                gifURL: responseGiphy.data.data[0].images.original.url
-              }
-              res.render('display', { dataToRender });
-            })
-            .catch(error => {
-              console.log({error, msg: 'Error getting data from Giphy'})
-            })
-        }
+        naturalLanguageUnderstanding.analyze(parameters, function(error, responseWatson) {
+          if (error) {
+            console.log({error, msg: 'Error getting data from Watson'});
+          } else {
+            const giphyKeyword = responseWatson.keywords[0].text;
+            console.log(giphyKeyword);
+            axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${giphyAPIkey}&q=${giphyKeyword}&limit=1`)
+              .then(responseGiphy => {
+                console.log(responseGiphy.data.data[0].images.original.url);
+                const dataToRender = {
+                  number,
+                  fact: responseTrivia.data,
+                  keyword: responseWatson.keywords[0].text,
+                  gifURL: responseGiphy.data.data[0].images.original.url
+                }
+                res.render('display', { dataToRender });
+              })
+              .catch(error => {
+                console.log({error, msg: 'Error getting data from Giphy'})
+              })
+          }
+      });
+    })
+    .catch(function (error) {
+      console.log({error, msg: 'Error getting data from Numbers API'});
     });
-  })
-  .catch(function (error) {
-    console.log({error, msg: 'Error getting data from Numbers API'});
-  });
+  }
 }
 
 module.exports = handleNumber;

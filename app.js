f5506b03259e71433a1f13496e2fb2827a70f573
Paul Acklam <paulacklam@gmail.com>
2018-09-07 12:11:06 +0100

    integrated the index.ejs home page

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 
-app.get('/api/:number', handleNumber);
+app.get('/', (req, res) => res.render('index'));
+
+app.get('/api', handleNumber);
 
 module.exports = app;
--- controller.js
+++ controller.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
+const giphyAPIkey = require('./giphy-config')
 
-// watson
+// watson setup
 const watsonConfig = require('./watson-config')
 const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1');
 const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
@@ -10,22 +11,11 @@ const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   url: 'https://gateway.watsonplatform.net/natural-language-understanding/api'
 });
 
-// giphy
-const giphyAPIkey = require('./giphy-config')
-
 const handleNumber = (req, res, next) => {
-  console.log(`req.params.number: ${req.params.number}`)
-  const number = parseInt(req.params.number);
-  console.log(number)
-  // if (/^\d+$/.test(number)) {
-  //   res.send({msg: `Your number is ${number}`})
-  // } else {
-  //   res.send({msg: `${req.params.number} is not a valid number!`})
-  // }
+  const number = parseInt(req.query.number);
 
   axios.get(`http://numbersapi.com/${number}`)
     .then(function (responseTrivia) {
-      // console.log(responseTrivia.data);
       var parameters = {
         'text': responseTrivia.data,
         'features': {
@@ -40,7 +30,6 @@ const handleNumber = (req, res, next) => {
         if (error) {
           console.log({error, msg: 'Error getting data from Watson'});
         } else {
-          // console.log(JSON.stringify(responseWatson.keywords, null, 2));
           const giphyKeyword = responseWatson.keywords[0].text;
           console.log(giphyKeyword);
           axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${giphyAPIkey}&q=${giphyKeyword}&limit=1`)
@@ -53,7 +42,6 @@ const handleNumber = (req, res, next) => {
                 gifURL: responseGiphy.data.data[0].images.fixed_width.url
               }
               res.render('display', { dataToRender });
-              // res.send(dataToRender)
             })
             .catch(error => {
               console.log({error, msg: 'Error getting data from Giphy'})

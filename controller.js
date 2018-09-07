fd193c29074178fce54f3e45e71258aa21c9600e
matt6gsr <matt6gsr@gmail.com>
2018-09-07 17:36:45 +0100

    heroku changes

--- controller.js
+++ controller.js
@@ -1,13 +1,17 @@
 const axios = require('axios');
-const giphyAPIkey = require('./giphy-config')
+const giphyAPIkey = require('./giphy-config');
+const { username, password } =
+  process.env.NODE_ENV === 'production'
+    ? process.env
+    : require('./watson-config');
 
 // watson setup
-const watsonConfig = require('./watson-config')
+//const watsonConfig = require('./watson-config');
 const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1');
 const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   version: '2018-03-16',
-  username: watsonConfig.username,
-  password: watsonConfig.password,
+  username,
+  password,
   url: 'https://gateway.watsonplatform.net/natural-language-understanding/api'
 });
 
@@ -16,25 +20,32 @@ const handleNumber = (req, res) => {
   if (!/^\d+$/.test(req.query.number)) {
     res.render('index');
   } else {
-    axios.get(`http://numbersapi.com/${number}`)
-      .then(function (responseTrivia) {
+    axios
+      .get(`http://numbersapi.com/${number}`)
+      .then(function(responseTrivia) {
         var parameters = {
-          'text': responseTrivia.data,
-          'features': {
-            'keywords': {
-              'emotion': false,
-              'sentiment': false,
-              'limit': 5
+          text: responseTrivia.data,
+          features: {
+            keywords: {
+              emotion: false,
+              sentiment: false,
+              limit: 5
             }
           }
-        }
-        naturalLanguageUnderstanding.analyze(parameters, function(error, responseWatson) {
+        };
+        naturalLanguageUnderstanding.analyze(parameters, function(
+          error,
+          responseWatson
+        ) {
           if (error) {
-            console.log({error, msg: 'Error getting data from Watson'});
+            console.log({ error, msg: 'Error getting data from Watson' });
           } else {
             const giphyKeyword = responseWatson.keywords[0].text;
             console.log(giphyKeyword);
-            axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${giphyAPIkey}&q=${giphyKeyword}&limit=1`)
+            axios
+              .get(
+                `http://api.giphy.com/v1/gifs/search?api_key=${giphyAPIkey}&q=${giphyKeyword}&limit=1`
+              )
               .then(responseGiphy => {
                 console.log(responseGiphy.data.data[0].images.original.url);
                 const dataToRender = {
@@ -42,19 +53,19 @@ const handleNumber = (req, res) => {
                   fact: responseTrivia.data,
                   keyword: responseWatson.keywords[0].text,
                   gifURL: responseGiphy.data.data[0].images.original.url
-                }
+                };
                 res.render('display', { dataToRender });
               })
               .catch(error => {
-                console.log({error, msg: 'Error getting data from Giphy'})
-              })
+                console.log({ error, msg: 'Error getting data from Giphy' });
+              });
           }
+        });
+      })
+      .catch(function(error) {
+        console.log({ error, msg: 'Error getting data from Numbers API' });
       });
-    })
-    .catch(function (error) {
-      console.log({error, msg: 'Error getting data from Numbers API'});
-    });
   }
-}
+};
 
 module.exports = handleNumber;
